0e68b590d7554473d0c668fa6d4d0b0d75d00c61
Jacob Littler <159176120+dankpompano@users.noreply.github.com>
2024-03-26 13:58:07 -0400

    Update movies.c
    added in search, delete, clear, printMovie, printMatches, printAll. Fixed formatting issues.

--- movies.c
+++ movies.c
@@ -10,7 +10,7 @@ int main()
 	}
 }
 
-
+//TREE STRUCTURE
 Movie* insert (Movie* root, char* name, int year, int minutes, int genre, long long revenue)
 {
 	if (root == NULL)
@@ -36,16 +36,84 @@ Movie* insert (Movie* root, char* name, int year, int minutes, int genre, long l
 	return root;
 }
 
-Movie* search (Movie* root, char* name, int year){
-	Movie* temp
-	
-	if(compare(root, name, year) )
-		return 
-	else
+Movie* search (Movie* root, char* name, int year)
+{
+	if(root == NULL)
+	{
 		return NULL;
+	}
 	
-	search(root->next, name, year);
+	else if(compare(root->name, name, year) == 0)
+	{
+		return root;
+	}
+	
+	else if(compare(root->name, name, year) < 0)
+	{
+		return search(root->left, name, year);
+	}
+	
+	else
+	{
+		return search(root->right, name, year);
+	}
 }
+
+Movie* delete (Movie* root, char* name, int year)
+{
+	if(root == NULL)
+	{
+		return NULL;
+	}
+	
+	else if(strcmp(name, root->name) == 0)
+	{
+		free(root);
+	}
+	//not finished.
+
+}
+
+void clear (Movie* root)
+{
+	if(root != NULL)
+	{
+		clear(root->left);
+		clear(root->right);
+		free(root->name);
+		free(root);
+	}
+}
+//
+//INFORMATION
+void printMovie(Movie* movie)
+{
+	if(movie->revenue == 0) //prints revenue as unknown
+	printf("Name:\t\t\t%s\n Year: \t\t\t%d\n Length:\t\t\t%d minutes\n Genre:\t\t\t%s\n Revenue:\t\t\tUnknown\n", movie->name, movie->year, movie->minutes, movie->genre);
+	else{
+	printf("Name:\t\t\t%s\n Year: \t\t\t%d\n Length:\t\t\t%d minutes\n Genre:\t\t\t%s\n Revenue:\t\t\t$%d\n", movie->name, movie->year, movie->minutes, movie->genre, movie->revenue);
+	}
+}
+
+int printMatches (Movie* root, char* name)
+{
+	if(root != NULL && strcmp(root->name, name) == 0)
+	{
+		printMatches(root->left);
+		printMovie(root);
+		printMatches(root->right);
+	}
+	return 0;
+}
+
+void printAll (FILE* file, Movie* root);
+//
+
+//STATISTICS
+int count (Movie* root);
+long long totalRevenue (Movie* root);
+Movie* highestGrossing (Movie* root);
+//
 
 int compare(Movie* movie, char* name, int year)
 {
